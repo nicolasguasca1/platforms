1103d8e883618929da9d77356efb221cb207e4d2
nicolasguasca1 <nicolas.guasca@gmail.com>
2022-02-01 00:26:03 +0100

    Typescript implemented

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,48 @@
+import NextAuth from "next-auth";
+import TwitterProvider from "next-auth/providers/twitter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+
+import type { NextAuthOptions } from "next-auth";
+
+if (!process.env.TWITTER_ID || !process.env.TWITTER_SECRET)
+  throw new Error("Failed to initialize Twitter authentication");
+
+export const authOptions = {
+  providers: [
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
+      profile(profile) {
+        return {
+          id: profile.id_str,
+          name: profile.name,
+          username: profile.screen_name,
+          email: profile.email && profile.email != "" ? profile.email : null,
+          image: profile.profile_image_url_https.replace(
+            /_normal\.(jpg|png|gif)$/,
+            ".$1"
+          )
+        };
+      }
+    })
+  ],
+  secret: process.env.SECRET,
+  pages: {
+    signIn: `/login`,
+    verifyRequest: `/login`
+  },
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+        username: user.username
+      }
+    })
+  }
+} as NextAuthOptions;
+
+export default NextAuth(authOptions);
---
+++ types/cloudinary.ts
@@ -0,0 +1,65 @@
+declare global {
+  var cloudinary: {
+    applyUploadWidget: (
+      element: unknown,
+      options: CloudinaryWidgetOptions,
+      widgetCallback?: Function
+    ) => void;
+    createUploadWidget: (
+      options: CloudinaryWidgetOptions,
+      widgetCallback?: Function
+    ) => CloudinaryWidget;
+    openUploadWidget: (
+      options: CloudinaryWidgetOptions,
+      widgetCallback?: Function
+    ) => void;
+    setAPIKey: (key: string) => void;
+    setCloudName: (name: string) => void;
+    WIDGET_SOURCES: CloudinaryWidgetSource;
+    WIDGET_VERSION: string;
+  };
+}
+
+enum CloudinaryWidgetSource {
+  CAMERA = "camera",
+  DROPBOX = "dropbox",
+  FACEBOOK = "facebook",
+  GETTY = "getty",
+  GOOGLE_DRIVE = "google_drive",
+  IMAGE_SEARCH = "image_search",
+  INSTAGRAM = "instagram",
+  ISTOCK = "istock",
+  LOCAL = "local",
+  SHUTTERSTOCK = "shutterstock",
+  UNSPLASH = "unsplash",
+  URL = "url"
+}
+
+export interface CloudinaryWidget {
+  close: (t?: unknown) => void;
+  destroy: (t?: unknown) => void;
+  hide: () => void;
+  isDestroyed: () => void;
+  isMinimized: () => void;
+  isShowing: () => void;
+  minimize: () => void;
+  open: (t?: unknown, e?: unknown) => void;
+  show: () => void;
+  update: (t?: unknown) => void;
+}
+
+interface CloudinaryWidgetOptions {
+  cloudName: string;
+  cropping: boolean;
+  uploadPreset: string;
+}
+
+export interface CloudinaryWidgetResult {
+  data: {
+    event: string;
+    info: string;
+  };
+  event: string;
+  info: string;
+  uw_event: boolean;
+}
