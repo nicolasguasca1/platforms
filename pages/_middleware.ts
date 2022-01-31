a428b8c2805ddd022757348fc62ff7bd1522da4a
nicolasguasca1 <nicolas.guasca@gmail.com>
2022-01-31 23:35:04 +0100

    First Changes

---
+++ pages/_middleware.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+
+import type { NextRequest } from "next/server";
+
+export default function middleware(req: NextRequest) {
+  // Get pathname of request (e.g. /blog-slug)
+  const { pathname } = req.nextUrl;
+
+  // Get hostname of request (e.g. demo.vercel.pub)
+  const hostname = req.headers.get("host");
+  if (!hostname)
+    return new Response(null, {
+      status: 400,
+      statusText: "No hostname found in request headers"
+    });
+
+  // Only for demo purposes – remove this if you want to use your root domain as the landing page
+  if (hostname === "vercel.pub")
+    return NextResponse.redirect("https://demo.vercel.pub");
+
+  // Strip root domain from hostname string
+  const currentHost = hostname.replace(
+    process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
+      ? `.vercel.pub`
+      : `.localhost:3000`,
+    ""
+  );
+
+  if (pathname.startsWith(`/_sites`))
+    return new Response(null, { status: 404 });
+
+  if (!pathname.includes(".") && !pathname.startsWith("/api")) {
+    if (currentHost == "app") {
+      if (
+        pathname === "/login" &&
+        (req.cookies["next-auth.session-token"] ||
+          req.cookies["__Secure-next-auth.session-token"])
+      )
+        return NextResponse.redirect("/");
+
+      return NextResponse.rewrite(`/app${pathname}`);
+    }
+
+    if (hostname === "localhost:3000") return NextResponse.rewrite(`/home`);
+
+    return NextResponse.rewrite(`/_sites/${currentHost}${pathname}`);
+  }
+}
